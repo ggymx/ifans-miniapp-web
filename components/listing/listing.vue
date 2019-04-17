3abb1962dac34c2ce6534b4e4639bfdb47a993e7
GeGanYmx <2319649532@qq.com>
2019-04-17 18:57:15 +0800

    init project

---
+++ components/listing/listing.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="item">
+    <span class="title">游戏和女友之间的战争</span>
+    <span class="context">女友删除男友游戏账号，男子怒砸电脑后跪地不起，网友称其太矫情，你觉得呢？</span>
+    <div class="status">
+      <img class="avatar">
+      <span class="name" style="margin-right:25px;">游哈哈</span>
+      <span class="date" style="white-space:pre-line;margin-right:40px;">2019.04.12 11.25</span>
+      <div style="display:inline-block;height:25px;display:flex;">
+          <img src="../../src/assets/post.png" style="width:21px;height:17px;margin-right:10px;margin-top: 5px;"/><!--
+          --><span style="margin-top:2px;">99人投稿</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "listing",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+ @import url(listing.css);
+</style>
---
+++ src/index.vue
@@ -0,0 +1,56 @@
+<template>
+<!--模板中只能包含一个标签-->
+<div>
+ <header>
+      <div>
+          <ul class="naviList">
+              <li style="margin-right:70px;">
+                <button class="logo-btn">
+                  <div><img src="./assets/topic.png"/></div>轻话题
+                  </button>
+              </li>
+              <li style="margin-right:90px"><a href="#" style="color:#666;text-decoration:none">热门话题</a></li>
+              <li style="margin-right:90px"><a href="#" style="color:#666;text-decoration:none">每日热点</a></li>
+              <li><a href="#" style="color:#666;text-decoration:none">我的首页</a></li>
+            </ul>
+            <button class="login-btn">登录</button>
+      </div> 
+    </header>
+    <!--话题+投稿-->
+    <article style="height:auto;width:100%;">
+      <div style="height:100%;width:1000px;margin:0 auto;display: flex">
+        <div style="height:100%;width:auto;padding-top:10px;">
+        <!--组件listing-->
+         <listing  v-for="(item,index) of arr" :key="index" style="margin-top:35px;"></listing>
+        </div><!--
+        --><div style="height:auto;width:auto;margin-left:10px;">
+            <div class="wechat-div">
+             <img src="./assets/topic-icon.png" style="width:94px,height:94px;margin-left:40px;margin-right:20px;"/>
+             <span>扫描二维码，<br/>打开轻话题小程序</span>
+            </div>
+         </div>
+      </div>
+    </article>
+   
+  </div>
+</template>
+
+<script>
+import listing from '../components/listing/listing.vue'
+export default {
+  name: 'index',
+  data () {
+    return {
+      arr: [1,2,3,4,5,7,8,9,10]
+    }
+  },
+  //使用组件
+  components:{
+    listing
+  }
+}
+</script>
+
+<style>
+@import url('index.css');
+</style>
